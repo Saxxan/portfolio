3142f104eba78f4430d1f0af137e1cbd51235d58
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-05-12 19:55:10 +0200

    trying header color changing

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,11 +1,21 @@
 import CTA from "../CTA/CTA";
 import Logo from "../logo/Logo";
 import NavMenu from "../navMenu/NavMenu";
-import "./header.css";
+import { useLocation } from 'react-router-dom';
+import './header.css';
 
-export default function Header(props) {
+export default function Header() {
+
+    const location = useLocation();
+
+    const isWhite = [
+    'design-projects',
+    'contact',
+    'detailed-design-project'
+    ].includes(location.pathname);
+
     return <header className="header">
-        <Logo></Logo>
+        <Logo class={isWhite ? "logo fillBlack" : "logo fillWhite"}></Logo>
         <NavMenu></NavMenu>
         <CTA name="Contact"></CTA>
     </header>
