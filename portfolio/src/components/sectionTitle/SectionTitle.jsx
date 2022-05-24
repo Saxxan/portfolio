0b0d7f98be5556f1f1ae411edef0867b54537cd9
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-05-24 09:18:49 +0200

    Section titles layout

--- portfolio/src/components/sectionTitle/SectionTitle.jsx
+++ portfolio/src/components/sectionTitle/SectionTitle.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components"
 
 const PageSectionTitle = styled.h2`
     width: 90vw;
-    margin: 0;
-    padding: 0;
+    margin: 0 auto;
+    padding: 15vh 10px 10px 10px;
     text-align: left;
     font-size: 2rem;
     font-weight: 500;
