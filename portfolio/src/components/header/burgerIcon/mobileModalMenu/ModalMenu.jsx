378fe8bbc03f6301cd019055d27ffc10e20cbedb
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-05-24 19:54:27 +0200

    modal styled components display action

--- portfolio/src/components/header/burgerIcon/mobileModalMenu/ModalMenu.jsx
+++ portfolio/src/components/header/burgerIcon/mobileModalMenu/ModalMenu.jsx
@@ -1,35 +1,30 @@
-// import React, { Children, createContext, useState } from 'react';
-// import { NavMenuMobile } from '../../../navMenu/NavMenu';
-// import styled from 'styled-components';
+import React, { useState } from 'react';
+import { NavMenuMobile } from '../../../navMenu/NavMenu';
+import styled from 'styled-components';
 
-// export const ModalMenuContext = createContext();
+const Modal = styled.div`
+    display: ${props => props.visible ? 'block' : 'none'};
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 3;
+    width: 100%;
+    height: 100%;
+    background: var(--bg-dark-1);
+`
+export default function ModalMenu(props) {
 
-// const Modal = styled.div`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   z-index: 3;
-//   width: 100%;
-//   height: 100%;
-//   background: var(--bg-dark-1);
-// `
-// export default function ModalMenu(props) {
+	const [isVisible, toggleVisibility] = useState(false);
 
-// 	const [isVisible, toggleVisibility] = useState(false);
+	function toggleModalState() {
+		toggleVisibility(!isVisible);
+	}
 
-// 	function toggleModalState() {
-// 		toggleVisibility(!isVisible);
-// 		isVisible ? this.style.display = 'block' : this.style.display = 'none';
-// 	}
-
-// 	return (
-// 		<ModalMenuContext.Provider>
-// 			<Modal>
-// 				<NavMenuMobile />
-// 			</Modal>
-// 			{Children}
-// 		</ModalMenuContext.Provider>
-// 	)
-// }
+	return (
+        <Modal visible={isVisible}>
+            <NavMenuMobile />
+        </Modal>
+	)
+}
 
 
