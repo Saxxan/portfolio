ff0944c6aba7ebef27e0d4adbc2b2e00f8e108d2
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-06-15 21:30:09 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode basename="salva-portfolio">
-    <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="salva-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
